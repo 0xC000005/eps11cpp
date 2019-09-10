98c36bb572547e263b3c0fac34f1e298118bc725
0xC00005 <hecknet@163.com>
2019-09-10 13:30:46 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex3/ch2ex4/ch2ex4/ch2ex4.cpp
@@ -0,0 +1,16 @@
+//Max Zhang - Sept. 9th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"Max Zhang"<<endl;
+	cout<<"High Schooler"<<endl;
+	cout<<"University of Leningrad"<<endl;
+	cout<<"Leningrad, USSR"<<endl;
+
+	return 0;
+}
