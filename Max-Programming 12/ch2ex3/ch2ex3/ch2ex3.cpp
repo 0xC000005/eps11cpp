952c9ffa13c228eb767098eac88b2bd2143974d4
0xC00005 <hecknet@163.com>
2019-09-10 13:26:38 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex3/ch2ex3/ch2ex3.cpp
@@ -0,0 +1,14 @@
+//Max Zhang - Sept. 9th
+#include<iostream>
+#include<conio.h> 
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+	cout << "Calculate the area of a circle of ";
+	cout << "Radius is " << 10;
+	cout <<  " is " << (3.14*10*10) << " . " << endl;
+	return 0;
+}
