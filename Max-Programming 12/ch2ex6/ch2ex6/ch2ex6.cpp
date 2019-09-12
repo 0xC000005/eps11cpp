164ab7f1566617d107f13eb47980a1c7b7789ea3
0xC00005 <hecknet@163.com>
2019-09-12 10:40:41 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex6/ch2ex6/ch2ex6.cpp
@@ -0,0 +1,14 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"the number of kilometers per liter a car gets if it travel "<<712.5<<"km on a "<<70.3<<" liter tank of gas:"<<endl;
+	cout<<712.5/70.3<<" l/km"<<endl;
+
+	return 0;
+}
---
+++ Max-Programming 12/ch2ex7/ch2ex7/ch2ex7.cpp
@@ -0,0 +1,17 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"The average for a long junmper with the forllowing is: \n"<<endl;
+	cout<<"Jump: "<<1<<" "<<3.3<<"m"<<endl;
+	cout<<"Jump: "<<2<<" "<<3.5<<"m"<<endl;
+	cout<<"Jump: "<<3<<" "<<4.0<<"m"<<endl;
+	cout<<"Jump: "<<4<<" "<<3.0<<"m"<<endl;
+	cout<<"The average jump is "<<(3.3+3.5+4.0+3.0)/4<<"m"<<endl;
+	return 0;
+}
