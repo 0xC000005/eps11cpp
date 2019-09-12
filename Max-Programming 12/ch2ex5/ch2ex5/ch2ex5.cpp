7447026826115244a4576f458ac63db43dacf121
0xC00005 <hecknet@163.com>
2019-09-12 10:38:21 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex5/ch2ex5/ch2ex5.cpp
@@ -0,0 +1,14 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"Area and perimeter of a rectangele with length "<<5<<" and width "<<3<<" is:"<<endl;
+	cout<<"area: "<<5*3<<endl;
+	cout<<"perimeter: "<<(5+3)*2<<endl;
+	return 0;
+}
