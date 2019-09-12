c05ffa79f5a3461be8bc59ff17dc09de4ea69ae2
0xC00005 <hecknet@163.com>
2019-09-12 10:42:18 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex8/ch2ex8/ch2ex8.cpp
@@ -0,0 +1,17 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	printf("%.d / %d = %.1f \n",6,5,6.0/5);
+	printf("%.1f / %d = %.1f \n",6.0,5,6.0/5);
+	printf("%d / %.1f = %.1f \n",6,5.0,6.0/5);
+	printf("%.1f / %.1f = %.1f \n",6.0,5.0,6.0/5);
+
+	return 0;
+}
