f95e0870f38f5b872ef371f18af5d46edf2d3329
0xC00005 <hecknet@163.com>
2019-09-13 13:54:25 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex9/ch2ex9/ch2ex9.cpp
@@ -0,0 +1,14 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"The number of students in programmming is following:\n Class 1 "<<14<<" people\n Class 2 "<<16<<" people \n Class 3 "<<13<<" people\n Class 4 "<<19<<" people"<<endl;
+	cout<<"The average size of class is "<<(14+16+13+19)/4.0<<endl;
+
+	return 0;
+}
