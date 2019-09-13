67b156f1dc9a4fd53702940d89abf0f577ca2e70
0xC00005 <hecknet@163.com>
2019-09-13 13:56:17 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex13/ch2ex13/ch2ex13.cpp
@@ -0,0 +1,22 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"    *****"<<endl;
+	cout<<"   *     *"<<endl;
+	cout<<"  *  \\ /  *"<<endl;
+	cout<<" *  o   o  *"<<endl;
+	cout<<"*     |     *"<<endl;
+	cout<<" *    +    *"<<endl;
+	cout<<"  *       *"<<endl;
+	cout<<"   *  O  *"<<endl;
+	cout<<"    *****"<<endl;
+	cout<<"  \"grouchy\""<<endl;
+
+	return 0;
+}
