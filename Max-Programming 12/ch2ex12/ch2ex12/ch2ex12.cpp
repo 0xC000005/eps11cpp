db12450fcf64a342dcb00c87bd801ebb808c1b4a
0xC00005 <hecknet@163.com>
2019-09-13 13:56:13 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex12/ch2ex12/ch2ex12.cpp
@@ -0,0 +1,22 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"    *****"<<endl;
+	cout<<"   *     *"<<endl;
+	cout<<"  * -   - *"<<endl;
+	cout<<" *  o   o  *"<<endl;
+	cout<<"*     |     *"<<endl;
+	cout<<" *    +    *"<<endl;
+	cout<<"  * \\___/ *"<<endl;
+	cout<<"   *     *"<<endl;
+	cout<<"    *****"<<endl;
+	cout<<"   \"happy\""<<endl;
+
+	return 0;
+}
