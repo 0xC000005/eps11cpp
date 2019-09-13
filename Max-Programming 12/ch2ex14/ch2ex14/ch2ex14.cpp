a3cc0f251e3650bd92354f85c0643cfa2fc9745d
0xC00005 <hecknet@163.com>
2019-09-13 13:56:23 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex14/ch2ex14/ch2ex14.cpp
@@ -0,0 +1,19 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"department  #pc"<<endl;
+	cout<<"Chemistry    "<<4<<endl;
+	cout<<"Physics      "<<8<<endl;
+	cout<<"English      "<<2<<endl;
+	cout<<"Math lab     "<<12<<endl;
+	cout<<endl;
+	cout<<"Grand Total  "<<4+8+2+12<<endl;
+	return 0;
+}
+
