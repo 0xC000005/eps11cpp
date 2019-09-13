6b5d7481629fcbd0156b15ed11b6eb64ba14deaf
0xC00005 <hecknet@163.com>
2019-09-13 13:56:00 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2ex10/ch2ex10/ch2ex10.cpp
@@ -0,0 +1,20 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"Year GHZ(the number of task a computer can process in one second.)"<<endl;
+	cout<<1980<<" "<<1<<endl;
+	cout<<1983<<" "<<4<<endl;
+	cout<<1986<<" "<<16<<endl;
+	cout<<1989<<" "<<64<<endl;
+	cout<<1992<<" "<<256<<endl;
+	cout<<1995<<" "<<1024<<endl;
+	cout<<1998<<" "<<4096<<endl;
+	cout<<2001<<" "<<16384<<endl;
+	return 0;
+}
