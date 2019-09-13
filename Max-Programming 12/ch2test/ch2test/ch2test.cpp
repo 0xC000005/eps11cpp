7efe5a5eb0387dfe49e752723a61608cdb0387c6
0xC00005 <hecknet@163.com>
2019-09-13 13:56:24 -0700

    Add files via upload

---
+++ Max-Programming 12/ch2test/ch2test/ch2test.cpp
@@ -0,0 +1,17 @@
+//Max Zhang - Sept. 10th
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"Old Navel Receipt\n"<<endl;
+	cout<<"Blouse  $"<<5.99<<endl;
+	cout<<"Jeans   $"<<14.99<<endl;
+	cout<<"Blet    $"<<6.99<<endl;
+	cout<<"Tax(12%)$"<<(5.99+14.99+6.99)*0.12<<"\n"<<endl;
+	cout<<"Final   $"<<(5.99+14.99+6.99)*1.12<<endl;
+	return 0;
+}
