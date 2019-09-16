6c1b5fcd5bf303df6708d3559f5dd65998bc1b3f
0xC00005 <hecknet@163.com>
2019-09-16 09:16:28 -0700

    Add files via upload

--- Max-Programming 12/ch2test/ch2test/ch2test.cpp
+++ Max-Programming 12/ch2test/ch2test/ch2test.cpp
@@ -7,11 +7,8 @@ using namespace std;
 int main()
 {
 	system("cls");
-	cout<<"Old Navel Receipt\n"<<endl;
-	cout<<"Blouse  $"<<5.99<<endl;
-	cout<<"Jeans   $"<<14.99<<endl;
-	cout<<"Blet    $"<<6.99<<endl;
-	cout<<"Tax(12%)$"<<(5.99+14.99+6.99)*0.12<<"\n"<<endl;
-	cout<<"Final   $"<<(5.99+14.99+6.99)*1.12<<endl;
+	cout<<"Number of People: "<<15<<endl;
+    cout<<"Price per Person: $"<<5.55<<endl;
+    cout<<"Total Sales:      $"<<83.25<<endl;
 	return 0;
 }
