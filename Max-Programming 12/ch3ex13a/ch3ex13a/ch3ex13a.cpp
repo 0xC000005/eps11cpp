845cbbe62c857e47121b02e8b2ef76137f3e30c2
0xC00005 <hecknet@163.com>
2019-09-23 09:04:16 -0700

    Add files via upload

---
+++ Max-Programming 12/ch3ex13a/ch3ex13a/ch3ex13a.cpp
@@ -0,0 +1,29 @@
+//Max Zhang - Sept. 19th
+#include<iostream>
+#include<conio.h>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	cout<<"Enter the number of burgers: ";
+	int burgers;
+	cin>>burgers;
+	cout<<"Enter the number of fries: ";
+	int fries;
+	cin>>fries;
+	cout<<"Enter the number of sodas: ";
+	int sodas;
+	cin>>sodas;
+	cout<<fixed<<setprecision(2)<<endl;
+	double total = burgers * 1.49 + fries * 0.89 + sodas * 0.99; 
+	cout<<"The total before tax is: $"<<total<<endl;
+	double tax = total * 0.06;
+	cout<<"The tax is $"<<tax<<endl;
+	double grand = total + tax;
+	cout<<"The grand total is $"<<grand<<endl;
+	return 0;
+}
