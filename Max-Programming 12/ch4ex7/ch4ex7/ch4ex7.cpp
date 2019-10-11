28b02d365770f1c68d9bcbd9914e627f97bbf352
0xC00005 <hecknet@163.com>
2019-10-11 14:11:22 -0700

    Mr. Tsuang's example sucks, try this one

---
+++ Max-Programming 12/ch4ex7/ch4ex7/ch4ex7.cpp
@@ -0,0 +1,26 @@
+﻿//Max Zhang - Oct. 11
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+	system("cls");
+	int num,digits=1;
+	cout<<"Enter an integer: ";
+	cin>>num;
+	while(num%(digits*10)!=num)digits*=10;
+	while(true)
+	{
+		int digit=num/digits;
+		cout<<digit<<endl;
+		num%=digits;
+		if(digits!=1)digits/=10;
+		else break;
+	}
+	return 0;
+}
+
+
+
