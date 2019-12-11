fe23e36fc55f596c0780751573a92257dc95bef7
0xC00005 <0xC00005@gmail.com>
2019-12-11 10:00:45 -0800

    update ScoreSheet.h

--- Max-Programming 12/Yathzee_beta/Yathzee_beta/Yathzee_beta/Yathzee_beta.cpp
+++ Max-Programming 12/Yathzee_beta/Yathzee_beta/Yathzee_beta/Yathzee_beta.cpp
@@ -4,258 +4,98 @@
 #include<vector>
 #include<iterator> 
 #include<conio.h>
-#include"Score.h"
+#include"ScoreSheet.h"
+#include<H:/lvp/RANDOM.H>
 using namespace std;
 
-class ScoreSheet
+class Game
 {
-	private:
-		vector<Score> score_paper;
-		string name;
-		string DESCRIPTIONS[16];
-		bool gameFinished;
-		int counter[6];
-        int result,sum;
-	public:
-		ScoreSheet()//init var
+	private: 
+		int dice[5];
+		int preserve[5];
+		int pickOut;
+		ScoreSheet player;
+    
+	public: 
+		Game() //init with description 
 		{
-			DESCRIPTIONS[0]="Ones";
-			DESCRIPTIONS[1]="Twos";
-			DESCRIPTIONS[2]="Threes";
-			DESCRIPTIONS[3]="Fours";
-			DESCRIPTIONS[4]="Fives";
-			DESCRIPTIONS[5]="Sixes";
-			DESCRIPTIONS[6]="Sum";
-			DESCRIPTIONS[7]="Bonus";
-			DESCRIPTIONS[8]="Three of a Kind";
-			DESCRIPTIONS[9]="Four of a Kind";
-			DESCRIPTIONS[10]="Full House";
-			DESCRIPTIONS[11]="Small Straight";
-			DESCRIPTIONS[12]="Large Straight";
-			DESCRIPTIONS[13]="Chance";
-			DESCRIPTIONS[14]="Yahtzee";
-			DESCRIPTIONS[15]="Total Score";
-			gameFinished=false;
-			result=0,sum=0;
-			init();
-		}   
+			pickOut=0;
 
-
-	void ScoreSheet::display(bool firstTime)
-    {
-        cout<<name<<"'s Score Sheet:"<<endl;
-		for(size_t i=0;i<score_paper.size();i++)
-        {
-			Score item=score_paper[i];
-            if(item.getDescription()=="Total Score")
-			{
-				cout<<item.getDescription()<<" "<<item.getScore()<<endl; // total score dont need markded sign
-			}
-            else
-            {
-                string marked;
-                if(item.getUsed()==1)
-                {
-                    //marked="☑";
-					marked="x";
-					cout<<marked<<" "<<item.getDescription()<<" "<<item.getScore()<<endl;
-                }
-                else
-                {
-                    //marked="☐";
-                    marked="o";
-					if(firstTime)
-					{
-						cout<<marked<<" "<<item.getDescription()<<" "<<item.getScore()<<endl;	
-					}
-                    else
-                    {
-                        if(item.getNum()==-1)//if ID is -1, means that is sum or total
-						{
-							cout<<marked<<" "<<item.getDescription()<<" "<<item.getScore()<<endl;	
-						}
-                        else
-                        {
-                            swicher(item.getNum());
-                            cout<<marked<<" "<<item.getDescription()<<" "<<sum<<endl;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    void ScoreSheet::init()
-    {	
-        cout<<"Please enter your name: ";
-        cin>>name;
-        bool bypass=false;//use to bypass Sum and Bouns at the first time
-        int count=0;//use count to replace swicher(int type)
-        for(int i=0;i<16;++i)
-        {
-            Score temp=Score(DESCRIPTIONS[i]);//init temp with description 
-            if(((count==6)||(count==7))&&bypass==false)//first time meet sum and bouns
-            {
-                temp.setNum(-1);//set ID=-1 for sum and bouns
-            }
-            else if(count==8&&bypass==false)
-            {
-                count-=2;
-                bypass=true;
-                temp.setNum(count);
-            }
-            else
-            {
-                temp.setNum(count);
-            }
-            score_paper.push_back(temp);
-            count++;
-        }
-        display(true);
-    }
-    
-   
-    
-    bool ScoreSheet::game_finished()
-    {
-        gameFinished=true;
-        for(size_t i=0; i<score_paper.size(); i++)
-        {
-			Score item=score_paper[i];
-            if(item.getUsed()==0)
-			{
-				gameFinished=false;
-			}
-            break;
-        }
-        return gameFinished;
-    }
-    
-    void ScoreSheet::counting(int dice[],int preserve[]) //make dices_list -> counter
-    {
-        for(int i=0;i<5;++i)
-        {
-            if(dice[i]!=0)
-			{
-				counter[dice[i]-1]++;
-			}
-            if(preserve[i]!=0)
-			{
-				counter[preserve[i]-1]++;
-			}
-        }
-
-    }
-    
-    void ScoreSheet::swicher(int type)//calculating the score/pantional score for SocreSheet
-    {
-		sum=0;//reset sum before swicher, IDK why lol
-
-        if(0<=type&&type<=5)sum=counter[type]*(type+1);//Ones~Sixes
-        else if(type==6)//Three of a kind
-        {
-            for(int i=0;i<5;++i)
-            {
-                bool statment=false;
-                sum+=counter[i]*(i+1);
-                if(counter[i]==3)
-                {
-                    statment=true;
-                    break;
-                }
-                if(!statment)sum=0;
-            }
-        }
-        else if(type==7)//Four of a kind
-        {
-            for(int i=0;i<5;++i)
-            {
-                bool statment=false;
-                sum+=counter[i]*(i+1);
-                if(counter[i]==4)
-                {
-                    statment=true;
-                    break;
-                }
-                if(!statment)sum=0;
-            }
-        }
-        else if(type==8)//Full house
-        {
-            bool two=false,three=false;
-            for(int i=0;i<5;++i)
-            {
-                sum+=counter[i]*(i+1);
-                if(counter[i]==2)two=true;
-                if(counter[i]==3)three=true;
-            }
-            if(!(two&&three))sum=0;
-        }
-        else if(type==9)//Small straight
-        {
-            for(int i=0;i<5;++i)sum+=counter[i]*(i+1);
-            bool case1=true,case2=true,case3=true;
-            for(int i=0;i<3;++i)
-            {
-                if(!(counter[i]>0&&counter[i+1]>0))case1=false;
-                if(!(counter[i+1]>0&&counter[i+2]>0))case2=false;
-                if(!(counter[i+2]>0&&counter[i+3]>0))case3=false;
-            }
-            if(!(case1||case2||case3))sum=0;
-        }
-        else if(type==10)//Large straight
-        {
-            bool statement=true;
-            for(int i=0;i<5;++i)
-            {
-                sum+=counter[i]*(i+1);
-                if(counter[i]!=1)statement=false;
-            }
-            if(!statement)sum=0;
-        }
-        else if(type==11)//Chance
-        {
-            for(int i=0;i<5;++i)
-            {
-                sum+=counter[i]*(i+1);
-            }
-        }
-        else if(type==12)//YAHTZEE
-        {
-            for(int i=0;i<5;++i)
-            {
-                if(counter[i]==5)sum=50;
-            }
-            if(sum!=50)sum=0;
-        }
-    }
-    
-    int ScoreSheet::getSum()
-    {
-        return sum;
-    }
-    
-    void ScoreSheet::player_action()
-    {
-        cout<<"Enter the option you want to mark this round: ";
-        string action;
-		cin>>action;
-        for(size_t i=0; i<score_paper.size(); i++)
-        {
-			Score item=score_paper[i];
-            if(action==item.getDescription())
-            {
-                swicher(item.getNum());
-                item.setScore(sum);
-                item.setUsed(item.getUsed()+1);
-            }
-        }
-    
+		}
+    void Game::print_dice(int number)
+	{
+		cout<<"Your dices:"<<endl;
+		cout<<number<<endl;
+		for(int i=0;i<number;++i)
+		{	
+			dice[i]=rand()%6+1;
+			cout<<dice[i]<<" ";
+		}
+		cout<<endl;
 	}
+
+	void Game::pressAnyKeyToContinue()
+	{ 
+		cout<<"Press Enter key to continue..."<<endl;
+		getchar();
+	}
+
+    
+	void Game::picking_out()
+	{
+		cout<<"Enter a number to select a dice you want to save."<<endl;
+		cout<<"Enter 0 to end the pick_out operation when you finished."<<endl;
+		int pick=-1;
+		while(pick!=0)
+		{
+			cout<<"Input:";
+			cin>>pick;
+			if(pick!=0)
+			{
+				for(int i=0;i<5;++i)
+				{
+					if(dice[i]==pick)
+					{
+						preserve[pickOut]=dice[i];
+						dice[i]=0;
+						pickOut++;
+						break;
+					}
+				}   
+			}
+		} 
+	}
+    
+	void Game::round()
+	{
+		bool finished=false;//after player_action(); return 0
+		for(int i=1;i<=3;++i)//each round has 3 tern
+		{
+			system("cls");//clear
+			player.display(false); //display new sheet
+			print_dice(5-pickOut);
+			picking_out();
+			player.count(dice,preserve);
+			system("cls");
+			player.display(false);
+			if(i<3)
+			{
+				cout<<"Do you want to use the score paper now? (0/1)";
+				int statment;
+				cin>>statment;
+				if(statment==1)
+				{
+					player.player_action();
+					break;
+				}
+			}
+			else if(i==3)
+			{
+				player.player_action();
+			}
+			player.display(false);
+		}
+	} 
+    
 };
 
-int main()
-{
-	ScoreSheet test;
-	return 0;
-}
