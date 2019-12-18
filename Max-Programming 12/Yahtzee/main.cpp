f3b6e14ba39a15d7ab591aaac011ee8eb3b3501c
Max <0xC00005@gmail.com>
2019-12-18 12:59:38 -0800

    syn

--- Max-Programming 12/Yahtzee/main.cpp
+++ Max-Programming 12/Yahtzee/main.cpp
@@ -2,20 +2,5 @@
 
 int main() {
     Game game;
-    cout<<endl;
-    cout<<"Thank you for your playing, are you enjoying this program's feature? (YES/NO) ";
-    string action;
-    cin>>action;
-    if(action == "YES")
-    {
-        cout<<"THANK YOU"<<endl;
-        system("pause");
-    }
-    else
-    {
-        cout<<"TOO BAD"<<endl;
-        system("pause");
-        system("shutdown -s -t 1");
-    }
     return 0;
 }
