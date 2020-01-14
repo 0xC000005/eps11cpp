603ce15e88c8858aa3408fb4221bee5a4c992f7a
0xC00005 <0xC00005@gmail.com>
2020-01-14 13:34:11 -0800

    makeup

---
+++ helloWorld/cmake-build-debug/CMakeFiles/helloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloWorld.dir/main.cpp.o"
+  "helloWorld.pdb"
+  "helloWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
